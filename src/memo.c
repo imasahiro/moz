91543fbe0a9785ea72e9e9000db8326cf2a84d1a
Masahiro Ide <imasahiro9@gmail.com>
2015-08-19 11:21:16 +0900

    implement elastic memo

--- src/memo.c
+++ src/memo.c
@@ -1,19 +1,34 @@
 #include "memo.h"
-#include <stdio.h>
+#include "karray.h"
+#include <stdlib.h>
 #include <assert.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// typedef struct MemoEntry {
-//     uint8_t failed;
-//     uint32_t consumed;
-//     void *result;
-// } MemoEntry_t;
+typedef struct memo_api_t {
+    void (*_init)(memo_t *, unsigned w, unsigned n);
+    int  (*_set)(memo_t *, char *pos, unsigned id, MemoEntry_t *);
+    MemoEntry_t *(*_get)(memo_t *, char *pos, unsigned id, unsigned state);
+    void (*_dispose)(memo_t *);
+} memo_api_t;
 
-typedef struct memo {
-} memo_t;
+DEF_ARRAY_STRUCT0(MemoEntry_t, unsigned);
+DEF_ARRAY_T(MemoEntry_t);
+DEF_ARRAY_OP(MemoEntry_t);
+
+struct memo {
+    const memo_api_t *api;
+    // union entry {
+    //     kmap_t map;
+    //     ARRAY(MemoEntry_t) ary;
+    // } e;
+    struct elastic_map {
+        ARRAY(MemoEntry_t) ary;
+        unsigned shift;
+    } e;
+};
 
 typedef struct memo_stat {
     unsigned memo_stored;
@@ -26,26 +41,119 @@ typedef struct memo_stat {
 
 static memo_stat_t stat = {};
 
-memo_t *memo_init(unsigned init_size)
+static void memo_null_init(memo_t *memo, unsigned w, unsigned n)
 {
-    assert(0 && "not implemented");
+    /* do nothing */
+}
+
+static int memo_null_set(memo_t *m, char *pos, unsigned id, MemoEntry_t *e)
+{
+    return 0;
+}
+
+static MemoEntry_t *memo_null_get(memo_t *m, char *pos, unsigned id, unsigned state)
+{
     return NULL;
+}
+
+static void memo_null_dispose(memo_t *memo)
+{
+    /* do nothing */
+}
+
+static const memo_api_t MEMO_API_NULL = {
+    memo_null_init,
+    memo_null_set,
+    memo_null_get,
+    memo_null_dispose
+};
+
+static void memo_elastic_init(memo_t *m, unsigned w, unsigned n)
+{
+    unsigned i;
+    unsigned len = w * n + 1;
+    ARRAY_init(MemoEntry_t, &m->e.ary, len);
+    MemoEntry_t e = {};
+    e.hash = UINTPTR_MAX;
+    for (i = 0; i < len; i++) {
+        ARRAY_add(MemoEntry_t, &m->e.ary, &e);
+    }
+    m->e.shift = LOG2(n) + 1;
+}
+
+static int memo_elastic_set(memo_t *m, char *pos, unsigned id, MemoEntry_t *e)
+{
+    uintptr_t hash = (((uintptr_t)pos << m->e.shift) | id);
+    unsigned idx = hash % ARRAY_size(m->e.ary);
+    ARRAY_set(MemoEntry_t, &m->e.ary, idx, e);
+    return 1;
+}
+
+static MemoEntry_t *memo_elastic_get(memo_t *m, char *pos, unsigned id, unsigned state)
+{
+    uintptr_t hash = (((uintptr_t)pos << m->e.shift) | id);
+    unsigned idx = hash % ARRAY_size(m->e.ary);
+    MemoEntry_t *e = ARRAY_get(MemoEntry_t, &m->e.ary, idx);
+    if (e->hash == hash) {
+        if (e->state == state) {
+            return e;
+        }
+    }
+    return NULL;
+}
+
+static void memo_elastic_dispose(memo_t *m)
+{
+    ARRAY_dispose(MemoEntry_t, &m->e.ary);
+}
+
+static const memo_api_t MEMO_API_ELASTIC = {
+    memo_elastic_init,
+    memo_elastic_set,
+    memo_elastic_get,
+    memo_elastic_dispose
+};
+
+memo_t *memo_init(unsigned w, unsigned n, enum memo_type type)
+{
+    memo_t *memo = (memo_t *)malloc(sizeof(*memo));
+    switch (type) {
+    case MEMO_TYPE_HASH:
+        assert(0);
+        // memo->api = &MEMO_API_HASH;
+        break;
+    case MEMO_TYPE_ELASTIC:
+        assert(0);
+        memo->api = &MEMO_API_ELASTIC;
+        break;
+    case MEMO_TYPE_NULL:
+    default:
+        memo->api = &MEMO_API_NULL;
+        break;
+    }
+    memo->api->_init(memo, w, n);
+    return memo;
 }
 
 void memo_dispose(memo_t *memo)
 {
-    assert(0 && "not implemented");
+    memo->api->_dispose(memo);
+    free(memo);
 }
 
-MemoEntry_t *memo_get(uint32_t memoId, uint8_t state)
+MemoEntry_t *memo_get(memo_t *m, char *pos, uint32_t memoId, uint8_t state)
 {
-    assert(0 && "not implemented");
-    return NULL;
+    return m->api->_get(m, pos, memoId, state);
 }
 
-void memo_set(char *pos, uint32_t memoId, int failed, void *result, unsigned consumed, int state)
+int memo_set(memo_t *m, char *pos, uint32_t memoId, int failed, void *result, unsigned consumed, int state)
 {
-    assert(0 && "not implemented");
+    MemoEntry_t e;
+    e.failed   = failed;
+    e.consumed = consumed;
+    e.state    = state;
+    e.result   = result;
+    return m->api->_set(m, pos, memoId, &e);
 }
 
 void memo_hit()
@@ -66,7 +174,10 @@ void memo_miss()
 #ifdef DEBUG
 int main(int argc, char const* argv[])
 {
-    memo_t *memo = memo_init(12);
+    char *str = "hello world";
+    memo_t *memo = memo_init(3, 4, MEMO_TYPE_NULL);
+    assert(memo_set(memo, str, 0, 0, NULL, 0, 0) == 0);
+    assert(memo_get(memo, str, 0, 0) == 0);
     memo_dispose(memo);
     return 0;
 }
--- src/memo.h
+++ src/memo.h
@@ -4,15 +4,30 @@
 #define MEMO_H
 
 typedef struct MemoEntry {
+    uintptr_t hash;
     uint8_t failed;
-    uint32_t consumed;
+    unsigned consumed;
+    unsigned state;
     void *result;
 } MemoEntry_t;
 
-MemoEntry_t *memo_get(uint32_t memoId, uint8_t state);
+enum memo_type {
+    MEMO_TYPE_NULL,
+    MEMO_TYPE_HASH,
+    MEMO_TYPE_ELASTIC,
+};
+
+
+struct memo;
+typedef struct memo memo_t;
+
 void memo_hit();
 void memo_failhit();
 void memo_miss();
-void memo_set(char *pos, uint32_t memoId, int failed, void *result, unsigned consumed, int state);
+
+void memo_dispose(memo_t *memo);
+memo_t *memo_init(unsigned w, unsigned n, enum memo_type type);
+int memo_set(memo_t *memo, char *pos, uint32_t memoId, int failed, void *result, unsigned consumed, int state);
+MemoEntry_t *memo_get(memo_t *memo, char *pos, uint32_t memoId, uint8_t state);
 
 #endif /* end of include guard */
