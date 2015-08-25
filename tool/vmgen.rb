597825710ffc60264ccb61695b36ba8e689d557d
Masahiro Ide <imasahiro9@gmail.com>
2015-08-25 10:10:50 +0900

    support CMakeLists.txt

--- tool/vmgen.rb
+++ tool/vmgen.rb
@@ -1,49 +1,54 @@
 #!ruby
 TAB="    "
 $line = 0
-FILE="src/instruction.def"
+
+file = ARGV[0]
+vmcore = ARGV[1]
+vminst = ARGV[2]
 
 types = []
-open(FILE) {|f|
+
+open(file) {|f|
+  out = open(vmcore, "w")
   while l = f.gets
     if l =~ /^DEF\((.*)\)/
       a = $1.split(",")
       op = a.shift
       # puts "#line #{$line} \"#{FILE}\""
-      puts "OP_CASE(#{op})\n{"
+      out.puts "OP_CASE(#{op})\n{"
       types << [op, *a.map{|e| e.split(" ")[0].strip }]
       a.each {|e|
         e = e.strip
         type, name = e.split(" ")
-        puts TAB + e + " = read_#{type}(PC);"
+        out.puts TAB + e + " = read_#{type}(PC);"
       }
     elsif l == "{\n"
     elsif l == "}\n"
-      puts TAB + "NEXT();"
-      puts "}"
+      out.puts TAB + "NEXT();"
+      out.puts "}"
     else
-      puts l
+      out.puts l
     end
     $line += 1
   end
-}
 
-f = open("src/vm_inst.h", "w")
-f.puts <<-TXT
+  out = open(vminst, "w")
+  out.puts <<-TXT
 #ifdef MOZVM_OPCODE_SIZE
 static unsigned opcode_size(int opcode)
 {
     switch (opcode) {
 TXT
-types.each {|e|
-  op = e[0]
-  size = ["MOZVM_INST_HEADER_SIZE", *(e[1..-1].map{|e| "sizeof(#{e})" })].join(" + ")
-  f.puts TAB + "case #{op}: return #{size};"
-}
-f.puts <<-TXT
+  types.each {|e|
+    op = e[0]
+    size = ["MOZVM_INST_HEADER_SIZE", *(e[1..-1].map{|e| "sizeof(#{e})" })].join(" + ")
+    out.puts TAB + "case #{op}: return #{size};"
+  }
+  out.puts <<-TXT
     default: break;
     }
     return -1;
 }
 #endif /*MOZVM_OPCODE_SIZE*/
 TXT
+}
