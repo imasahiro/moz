64e08ad96afdbdc2ce691fa83a6856b0e0b79747
Masahiro Ide <imasahiro9@gmail.com>
2015-08-26 22:54:44 +0900

    update check_size.sh

--- tool/check_size.sh
+++ tool/check_size.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-make -C Release
-./Release/moz_stat -s -q -n 1 -p sample/js.nzc -i nez-sample/_/js/jquery-2.1.1.js
-strip ./Release/moz
-du -h ./Release/moz
+make -C Release clean
+make -C Release >/dev/null 2>&1
+time -p ./Release/moz_stat -s -q -n 1 -p sample/json.nzc -i test_vm/bench/input/benchmark4.json
+echo
+time -p ./Release/moz_stat -s -q -n 1 -p sample/js.nzc -i nez-sample/_/js/jquery-2.1.1.js
+echo -n "linux gcc4.7.2 x86 -Os      :"
+du -h ./Release/moz_all
+strip ./Release/moz_all
+echo -n "linux gcc4.7.2 x86 -Os+strip:"
+du -h ./Release/moz_all
