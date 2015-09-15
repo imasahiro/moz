59662533242a1076088b7337eac10e0f1c8336ff
Masahiro Ide <imasahiro9@gmail.com>
2015-09-15 19:10:41 +0900

    update tools

--- tool/bench.sh
+++ tool/bench.sh
@@ -8,17 +8,18 @@ LOOP=5
 if [ $# = 1 ]; then
     LOOP=$1
 fi
+EXT=moz
 BENCH=test_vm/bench/input
 CMD="./Release/moz -n ${LOOP} -q -s "
 # CMD2="./Release/moz_profile -n 1 -q -s "
 echo "citys.json"
-${CMD} -p sample/old_json.ncz -i ${BENCH}/citys.json
+${CMD} -p sample/old_json.${EXT} -i ${BENCH}/citys.json
 echo "earthquake.geojson"
-${CMD} -p sample/old_json.ncz -i ${BENCH}/earthquake.geojson
+${CMD} -p sample/old_json.${EXT} -i ${BENCH}/earthquake.geojson
 echo "benchmark4.json"
-${CMD} -p sample/old_json.ncz -i ${BENCH}/benchmark4.json
+${CMD} -p sample/old_json.${EXT} -i ${BENCH}/benchmark4.json
 echo "xmark5m.xml"
-${CMD} -p sample/xml.ncz  -i ${BENCH}/xmark5m.xml
+${CMD} -p sample/xml.${EXT}  -i ${BENCH}/xmark5m.xml
 
-# ${CMD2} -p sample/old_json.ncz -i ${BENCH}/earthquake.geojson
-# ${CMD2} -p sample/xml.ncz  -i ${BENCH}/xmark5m.xml
+# ${CMD2} -p sample/old_json.${EXT} -i ${BENCH}/earthquake.geojson
+# ${CMD2} -p sample/xml.${EXT}  -i ${BENCH}/xmark5m.xml
--- tool/gen.sh
+++ tool/gen.sh
@@ -4,5 +4,6 @@ NEZOPT=--option:+ast:+memo:+predict:+str
 NEZ=../nez.jar
 
 for i in sample/*.nez; do
+    echo $i
     java -jar ${NEZ} compile ${NEZOPT} -p $i
 done
