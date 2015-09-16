847aae117bd904b57690ebed24c27ff35c747fc9
Masahiro Ide <imasahiro9@gmail.com>
2015-09-16 21:57:32 +0900

    fix build error

--- src/stat.cpp
+++ src/stat.cpp
@@ -3,16 +3,17 @@
 // #define MOZVM_PROFILE_INST  1
 
 #include "mozvm_config.h"
-#include "node.c"
+
 #include "ast.c"
 #include "symtable.c"
 #include "memo.c"
-
-#include "memory.c"
 #include "loader.c"
-#include "vm.c"
-#include "main.c"
 
 #ifdef MOZVM_ENABLE_JIT
 #include "jit.cpp"
 #endif
+
+#include "node.c"
+#include "memory.c"
+#include "vm.c"
+#include "main.c"
