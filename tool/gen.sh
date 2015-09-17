2da0dbe4d5093a8785cd3472ba7a66e519a60664
Masahiro Ide <imasahiro9@gmail.com>
2015-09-17 17:26:53 +0900

    update gen.sh

--- tool/gen.sh
+++ tool/gen.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 NEZOPT=--option:+ast:+memo:+predict:+str
-NEZ=../nez.jar
+NEZ=../../nez.jar
 
-for i in sample/*.nez; do
+cd sample
+for i in *.nez; do
     echo $i
     java -jar ${NEZ} compile ${NEZOPT} -p $i
 done
