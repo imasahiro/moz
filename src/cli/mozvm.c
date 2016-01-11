657cefd7bc8f4db7e071e45912807889695d875d
Masahiro Ide <imasahiro9@gmail.com>
2016-01-11 17:19:10 +0900

    Add integration tests

--- src/cli/mozvm.c
+++ src/cli/mozvm.c
@@ -30,16 +30,73 @@ static int parse(moz_module_t *M, const char *input_file)
     return result;
 }
 
-int main(int argc, char *const argv[])
+struct parse_result {
+    const char *error;
+    int parsed;
+};
+
+static int run(const char *peg_file, const char *input_file, struct parse_result *result)
 {
-    long parsed;
     mozvm_loader_t L;
     moz_inst_t *inst;
+    Node *node;
 
+    if (input_file == NULL) {
+        result->error = "input_file is null. Please specify input file name";
+        result->parsed = 0;
+        return 1;
+    }
+    if (peg_file == NULL) {
+        result->error = "peg_file is null. Please specify input peg name";
+        result->parsed = 0;
+        return 1;
+    }
+    if (!mozvm_loader_load_input_file(&L, peg_file)) {
+        result->error = "Failed to load peg file";
+        result->parsed = 0;
+        return 1;
+    }
+    inst = mozvm_loader_load_syntax(&L, syntax_bytecode,
+            sizeof(syntax_bytecode), 1);
+    assert(inst != NULL);
+
+    NodeManager_init();
+
+    moz_runtime_set_source(L.R, L.input, L.input + L.input_size);
+    inst = moz_runtime_parse_init(L.R, L.input, inst);
+    if (moz_runtime_parse(L.R, L.input, inst) != 0) {
+        result->error = "Failed to load peg file";
+        result->parsed = 0;
+        goto L_finally;
+    }
+    node = ast_get_parsed_node(L.R->ast);
+    if (node == NULL) {
+        result->error = "Failed to parse peg file";
+        result->parsed = 0;
+        goto L_finally;
+    }
+    moz_module_t *M = moz_compiler_compile(L.R, node);
+    NODE_GC_RELEASE(node);
+    if (parse(M, input_file) == 0) {
+        result->error = "Failed to parse input file";
+        result->parsed = 0;
+        goto L_finally;
+    }
+
+    result->parsed = 1;
+L_finally:
+    moz_runtime_dispose(L.R);
+    mozvm_loader_dispose(&L);
+    NodeManager_dispose();
+    return result->parsed == 1;
+}
+
+int main(int argc, char *const argv[])
+{
     const char *peg_file = NULL;
     const char *input_file = NULL;
+    struct parse_result result = {};
     int opt;
-    Node *node;
 
     while ((opt = getopt(argc, argv, "p:i:h")) != -1) {
         switch (opt) {
@@ -55,50 +112,9 @@ int main(int argc, char *const argv[])
             exit(EXIT_FAILURE);
         }
     }
-    if (input_file == NULL) {
-        fprintf(stderr, "error: please specify input file name\n");
-        usage(argv[0]);
-        exit(EXIT_FAILURE);
+    if (run(peg_file, input_file, &result) == 0) {
+        fprintf(stderr, "Error: %s\n", result.error);
     }
-    if (peg_file == NULL) {
-        fprintf(stderr, "error: please specify peg file\n");
-        usage(argv[0]);
-        exit(EXIT_FAILURE);
-    }
-    if (!mozvm_loader_load_input_file(&L, peg_file)) {
-        fprintf(stderr, "error: failed to load peg file='%s'\n", peg_file);
-        usage(argv[0]);
-        exit(EXIT_FAILURE);
-    }
-    inst = mozvm_loader_load_syntax(&L, syntax_bytecode,
-            sizeof(syntax_bytecode), 1);
-    assert(inst != NULL);
-
-    NodeManager_init();
-
-    moz_runtime_set_source(L.R, L.input, L.input + L.input_size);
-    inst = moz_runtime_parse_init(L.R, L.input, inst);
-    parsed = moz_runtime_parse(L.R, L.input, inst);
-    if (parsed != 0) {
-        fprintf(stderr, "peg parse error\n");
-        goto L_finally;
-    }
-    node = ast_get_parsed_node(L.R->ast);
-    if (node == NULL) {
-        fprintf(stderr, "peg parse error\n");
-        goto L_finally;
-    }
-    moz_module_t *M = moz_compiler_compile(L.R, node);
-    NODE_GC_RELEASE(node);
-    if (parse(M, input_file) == 0) {
-        fprintf(stderr, "peg parse error\n");
-    }
-
-L_finally:
-    moz_runtime_dispose(L.R);
-    mozvm_loader_dispose(&L);
-    NodeManager_dispose();
-    return 0;
 }
 
 #ifdef __cplusplus
