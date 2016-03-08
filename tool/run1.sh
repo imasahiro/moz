11e45166dc46826d6c68899ecd1e5a37987f835c
Masahiro Ide <imasahiro9@gmail.com>
2016-03-09 00:30:51 +0900

    Update tools

--- tool/run1.sh
+++ tool/run1.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-make -C Debug clean
+# make -C Debug clean
 make -C Debug
 FILE=$1
-TXT=$2
-# M=valgrind --leak-check=full
+M="valgrind --leak-check=full"
+M="lldb --"
 # java -jar ../nez.jar compile -p $FILE
 
 # lldb -- ./Debug/moz -p ${FILE%.*}.ncz -i $2
 # ./Debug/moz -p ${FILE%.*}.ncz -i $2 >& out
-valgrind --leak-check=full ./Debug/moz -p ${FILE%.*}.ncz -i $2
+
+$M ./Debug/mozvm -p $FILE -i ${FILE%.*}.txt
